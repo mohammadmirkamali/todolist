c7952d1c38dd2dc88e67126f8ca0bb6369b34e2d
Berlin <hamedrajabpoor724@gmail.com>
2022-04-06 11:11:57 +0430

    change request library to axios

--- services/request.ts
+++ services/request.ts
@@ -1,43 +1,26 @@
-import { ApisauceInstance, create } from 'apisauce';
-import { parseCookies } from 'nookies';
+import Axios from 'axios';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'https://taalei-edu.ir';
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'https://taalei-edu.com';
 const newURL = process.env.NEXT_PUBLIC_NEW_URL || 'https://api.taalei-edu.com';
 
-// create main request configs
-const request = ((): ApisauceInstance => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Accept-Language': 'fa',
-  };
-
-  // eslint-disable-next-line no-underscore-dangle
-  const _api = create({
-    baseURL,
-    headers,
-  });
-
-  return _api;
-})();
-
-request.addRequestTransform((req) => {
-  // eslint-disable-next-line no-underscore-dangle
-  const _token = parseCookies().taalei;
-  if (_token) {
-    req.headers.authorization = `Bearer ${_token}`;
-  }
+const request = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+  withCredentials: true,
 });
 
-request.axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response.status !== 401) {
-      return error;
-    }
+request.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (error.response.status !== 401) {
+        return error;
+      }
 
-    return error;
-  },
+      return error;
+    },
 );
 
 export default request;
