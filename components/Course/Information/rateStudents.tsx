6c64968da5ea23b80627fd921048ef537d819cd4
m.mirkamali <mirrkamali@gmail.com>
2022-06-30 10:24:13 +0430

    fix1

--- components/Course/Information/rateStudents.tsx
+++ components/Course/Information/rateStudents.tsx
@@ -1,44 +1,44 @@
 import { StarFilled, UserOutlined } from '@ant-design/icons';
-// import { Avatar } from 'antd';
-// import AntTooltip from 'components/Common/AntTooltip';
-// import { t } from 'i18next';
-// import React from 'react';
-// import ScrollContainer from 'react-indiana-drag-scroll';
-// import { TopRateType } from 'types/course.type';
-// import { faNumber } from 'utils/common.util';
+import { Avatar } from 'antd';
+import AntTooltip from 'components/Common/AntTooltip';
+import { t } from 'i18next';
+import React from 'react';
+import ScrollContainer from 'react-indiana-drag-scroll';
+import { TopRateType } from 'types/course.type';
+import { faNumber } from 'utils/common.util';
 
-// const RateStudents: React.FC<{ data: TopRateType[]; top?: boolean }> = ({
-//   data,
-//   top,
-// }) => (
-//   <div className="py-[7px] flex text-[16px] flex-col pr-[15px] overflow-hidden w-full">
-//     <p className="m-0 mb-[8px]">{t(`course.${top ? 'rateStudents' : 'yourRate'}`)}</p>
-//     <ScrollContainer className="flex">
-//       {data.map((item) => (
-//         <div
-//           key={item.id}
-//           className=" flex flex-col ml-[30px] items-center max-w-[100px] text-[14px] shrink-0 text-center"
-//         >
-//           {item.avatar ? (
-//             <Avatar src={item.avatar} alt="avatar" />
-//           ) : (
-//             <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
-//           )}
+const RateStudents: React.FC<{ data: TopRateType[]; top?: boolean }> = ({
+  data,
+  top,
+}) => (
+  <div className="py-[7px] flex text-[16px] flex-col pr-[15px] overflow-hidden w-full">
+    <p className="m-0 mb-[8px]">{t(`course.${top ? 'rateStudents' : 'yourRate'}`)}</p>
+    <ScrollContainer className="flex">
+      {data.map((item) => (
+        <div
+          key={item.id}
+          className=" flex flex-col ml-[30px] items-center max-w-[100px] text-[14px] shrink-0 text-center"
+        >
+          {item.avatar ? (
+            <Avatar src={item.avatar} alt="avatar" />
+          ) : (
+            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+          )}
 
-//           <div className="toRight">
-//             <AntTooltip name={item.nickname || ''} length={13}>
-//               {item.nickname}
-//             </AntTooltip>
+          <div className="toRight">
+            <AntTooltip name={item.nickname || ''} length={13}>
+              {item.nickname}
+            </AntTooltip>
 
-//             <div className="text-[12px]">
-//               ( {faNumber(item.total_rate)} <StarFilled className="translate-y-[-2px] " />{' '}
-//               )
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//     </ScrollContainer>
-//   </div>
-// );
+            <div className="text-[12px]">
+              ( {faNumber(item.total_rate)} <StarFilled className="translate-y-[-2px] " />{' '}
+              )
+            </div>
+          </div>
+        </div>
+      ))}
+    </ScrollContainer>
+  </div>
+);
 
-// export default RateStudents;
+export default RateStudents;
